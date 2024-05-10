b5ec0e526869b8a5f034cfad395f0531f65a6dca
NathBo <nboyer200492@gmail.com>
2024-05-10 11:14:50 +0200

    assign fait dans domain.ml

--- domains/domain.ml
+++ domains/domain.ml
@@ -7,58 +7,77 @@
 open Frontend
 open! Cfg
 
+module VD = Value_domain.VALUE_DOMAIN
+
 (* Signature for the variables *)
 
 module type VARS = sig
   val support: var list
 end
 
+module Env = Map.Make( 
+  struct
+    let compare = compare
+    type t = var
+  end )
+
 (*
   Signature of abstract domains representing sets of envrionments
   (for instance: a map from variable to their bounds).
  *)
 
-module type DOMAIN =
-  sig
+
+module DOMAIN =
+  struct
 
     (* type of abstract elements *)
     (* an element of type t abstracts a set of mappings from variables
        to integers
      *)
-    type t
+    type t = VD.t Env.t
 
     (* initial environment, with all variables initialized to 0 *)
-    val init: t
+    let init (var_list:var list) = Env.of_list (List.map (fun x -> (x,VD.const 0)) var_list)
 
     (* empty set of environments *)
-    val bottom: t
+    let bottom = Env.empty
+
+
+    let rec evaluate env int_expr = match int_expr with
+      | CFG_int_const n -> VD.const (Z.to_int n)
+      | CFG_int_var v -> Env.find v env
+      | CFG_int_unary (op,i) -> VD.unary (evaluate env i) op
+      | CFG_int_binary (op,i1,i2) -> VD.binary (evaluate env i1) (evaluate env i2) op
+      | CFG_int_rand (n1,n2) -> VD.rand (Z.to_int n1) (Z.to_int n2)
+
 
     (* assign an integer expression to a variable *)
-    val assign: t -> var -> int_expr -> t
+    let assign env var int_expr = 
+      Env.add var (evaluate env int_expr) env
 
     (* filter environments to keep only those satisfying the boolean expression *)
-    val guard: t -> bool_expr -> t
+    let guard: t -> bool_expr -> t
 
     (* abstract join *)
-    val join: t -> t -> t
+    let join: t -> t -> t
 
     (* abstract meet *)
-    val meet: t -> t -> t
+    let meet: t -> t -> t
 
     (* widening *)
-    val widen: t -> t -> t
+    let widen: t -> t -> t
 
     (* narrowing *)
-    val narrow: t -> t -> t
+    let narrow: t -> t -> t
 
     (* whether an abstract element is included in another one *)
-    val subset: t -> t -> bool
+    let subset: t -> t -> bool
 
     (* whether the abstract element represents the empty set *)
-    val is_bottom: t -> bool
+    let is_bottom: t -> bool
 
     (* prints *)
-    val print: Format.formatter -> t -> unit
+    let print: Format.formatter -> t -> unit
 
   end
 
